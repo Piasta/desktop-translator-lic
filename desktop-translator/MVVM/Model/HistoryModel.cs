77ab62a3faf99961b7acacff4dbe2f72d90b8231
ppiasta <ppiasta@WA2001LN0731>
2023-02-14 16:29:03 +0100

    database connection

--- desktop-translator/MVVM/Model/HistoryModel.cs
+++ desktop-translator/MVVM/Model/HistoryModel.cs
@@ -1,5 +1,9 @@
 ﻿using desktop_translator.Core;
 using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Net;
 using System.Windows;
 
@@ -7,9 +11,28 @@ namespace desktop_translator.MVVM.Model
 {
     class HistoryModel : ObservableObject
     {
+      
         public void Test()
         {
-            MessageBox.Show("Work!");
+
+            string dbPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "HistoryDB.db");
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=" + dbPath + ";Version=3;");
+
+            m_dbConnection.Open();
+
+            if (m_dbConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("OPEN");
+                string sql = "insert into Phrases (TypedText, TranslatedText) values ('WpisaneZ', 'Kodu')";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                MessageBox.Show("Close");
+            }
+
         }
     }
 }
