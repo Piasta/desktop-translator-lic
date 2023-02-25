ccbf1c9d94b8ce4c88f774526399c607c32cd6d8
Piasta <Piasta@DESKTOP-8P0LS9T>
2023-02-25 23:36:11 +0100

    auto lang detection

--- desktop-translator/MVVM/Model/TranslateModel.cs
+++ desktop-translator/MVVM/Model/TranslateModel.cs
@@ -2,6 +2,8 @@
 using desktop_translator.MVVM.ViewModel;
 using System;
 using System.Net;
+using System.Security.Policy;
+using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
@@ -35,13 +37,28 @@ namespace desktop_translator.MVVM.Model
             }
         }
 
+        private Boolean _isChecked = true;
+        public Boolean IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                _isChecked = value;
+                OnPropertyChanged("IsChecked");
+            }
+        }
+
         public void Translate()
         {
             if (!string.IsNullOrEmpty(RawText))
             {
                 var toLanguage = "en";
                 var fromLanguage = "pl";
-                
+
+                if (IsChecked == true)
+                {
+                    fromLanguage = "auto";
+                }
                 try
                 {
                     var url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={WebUtility.UrlEncode(RawText)}";
@@ -49,11 +66,11 @@ namespace desktop_translator.MVVM.Model
                     {
                         Encoding = System.Text.Encoding.UTF8
                     };
-
                     var result = webClient.DownloadString(url);
                     result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+
                     TranslatedText = result;
-                }
+                    }
                 catch
                 {
                     MessageBox.Show("NoNetwork");
